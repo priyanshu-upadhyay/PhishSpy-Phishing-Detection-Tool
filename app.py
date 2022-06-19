2b330cde65341fdc14cf49ffe6a7332a9ae35ffa
Priyanshu Upadhyay <priyanshu.upadh@gmail.com>
2022-06-20 01:11:20 +0530

    API Created

---
+++ app.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, request, json
+from main import main
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/checkphishing', methods=["POST"])
+def checkphishing():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        if "url" not in json.keys():
+            return {"message" : "parameter missing"}
+        url = json["url"]
+        try:
+            response = main(url)
+            return response
+        except:
+            return {"status-code" : -1, "message" : "Internal Error, admin is notified"}
+    else:
+        return "Content-Type not supported!"
+
+@app.route('/count')
+def checkcount():
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+    cur.execute("select count from phishcount where key=1")
+    curr_count = cur.fetchall()[0][0]
+    conn.close()
+    return {"status" : 200, "count": curr_count}
+
+
+
+
+
+if __name__ == '__main__':
+
+	app.run(debug=True)
--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-
 import requests
 import re, tldextract, whois, favicon, socket #pip install python-whois
 import dns.resolver #pip install dnspython
@@ -12,89 +11,95 @@ suspectScore = 0
 #for low and high severity suspecious : Score = Score + 1
 
 #1
-def checkIp(url):
+def checkIp(url):   #not critical (medium)
     global suspectScore
     checkIp=re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",url) or re.search("(([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?([a-zA-Z]+([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?)+)",url)
-    if checkIp:
-        suspectScore=suspectScore+1
+    if checkIp: # Ye Phishing Hai
+        suspectScore = suspectScore + 3
         return True
     else:
         return False
 #2
-def checkUrlLength(url):
+def checkUrlLength(url):    
     global suspectScore
     if len(url) < 54:
         return False
     elif(len(url)>=54 and len(url)<=75):
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+2
         return True
     else:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+4
         return True
 #3
-def checkUrlSymbol(url):
+def checkUrlSymbol(url):    #critical
     global suspectScore
     if "@" in url:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+5
         return True
     else:
         return False
 #4
-def checkDomainHyphen(url):
+def checkDomainHyphen(url):     #not critical
     global suspectScore
     ext = (tldextract.extract(url)).domain
     if "-" in ext:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+2
         return True
     else:
         return False
 #5
-def checkDomainAge(url):
+def checkDomainAge(url):       #critical
     global suspectScore
-    w = whois.whois(url)
-    w = w['creation_date'][0]
-    now = datetime.datetime.today()
-    days = int((str((now - w)).split())[0])
-    if days < 182:
-        suspectScore=suspectScore+1
-        return True
-    else:
+    try:
+        w = whois.whois(url)
+        w = w['creation_date']
+        now = datetime.datetime.today()
+        days = (now - w).days
+        if days < 182:
+            suspectScore=suspectScore+5
+            return True
+        else:
+            return False
+    except:
         return False
 #6
-def checkDomainExpiry(url):
+def checkDomainExpiry(url):     #non critical
     global suspectScore
-    w = whois.whois(url)
-    w = w['expiration_date'][0]
-    now = datetime.datetime.today()
-    days = int((str((w - now)).split())[0])
-    if days < 365:
-        suspectScore=suspectScore+1
-        return True
-    else:
+    try:
+        w = whois.whois(url)
+        w = w['creation_date']
+        now = datetime.datetime.today()
+        days = (now - w).days
+        if days < 365:
+            suspectScore=suspectScore+4
+            return True
+        else:
+            return False
+    except:
         return False
 
 #7
-def checkFaviconSource(url):
+def checkFaviconSource(url):        #critical
     global suspectScore
     givenUrl = (tldextract.extract(url)).domain
     icon = (tldextract.extract(favicon.get(url)[0][0])).domain
     if givenUrl != icon:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+5
         return True
     else:
         return False
 #8
-def checkShortenUrl(url):
+def checkShortenUrl(url):       #not critical
     global suspectScore
     sProviders=['T.LY', 'bit.ly', 'is.gd', 'Ow.ly', 'shrunken.com', 'p.asia', 'g.asia', '3.ly', '0.gp', '2.ly', '4.gp', '4.ly', '6.ly', '7.ly', '8.ly', '9.ly', '2.gp', '6.gp', '5.gp', 'ur3.us', 'tiny.cc', 'soo.gd', 'clicky.me', 'bl.ink', 'buff.ly', 'rb.gy', 't2mio', 'bit.do', 'cutt.ly', 'shorturl.at', 'urlzs.com', 'LinkSplit', 'short.io', 'kutt.it', 'switchy.io', 'han.gl', 'lh.ms']
     for i in sProviders:
         if i in url:
-            suspectScore=suspectScore+1
+            suspectScore=suspectScore+3
             return True
         else:
             return False
 #9
-def checkOpenPorts(url):
+def checkOpenPorts(url):        #not critical
     global suspectScore
     if(tldextract.extract(url).subdomain):
         ip=tldextract.extract(url).subdomain+"."+tldextract.extract(url).domain+"."+tldextract.extract(url).suffix
@@ -113,36 +118,36 @@ def checkOpenPorts(url):
         except:
             continue
     if(OpenPorts!=OpenPortsShouldBe):
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+3
         return True
     else:
         return False
 #10
-def checkHttpsInDomain(url):
+def checkHttpsInDomain(url):        #critical
     global suspectScore
     if(tldextract.extract(url).subdomain):
         url=tldextract.extract(url).subdomain+"."+tldextract.extract(url).domain+"."+tldextract.extract(url).suffix
     else:
         url=tldextract.extract(url).domain+"."+tldextract.extract(url).suffix
     if "https" in url:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+5
         return True
     else:
         return False
 #11
-def checkDNSRecord(url):
+def checkDNSRecord(url):        #critical
     global suspectScore
     if(tldextract.extract(url).subdomain):
         url=tldextract.extract(url).subdomain+"."+tldextract.extract(url).domain+"."+tldextract.extract(url).suffix
     else:
         url=tldextract.extract(url).domain+"."+tldextract.extract(url).suffix
-    if(dns.resolver.resolve(url, 'MX') or dns.resolver.resolve(url, 'A') or dns.resolver.resolve(url, 'NS') ):
+    if(dns.resolver.resolve(url, 'MX') or dns.resolver.resolve(url, 'A') or dns.resolver.resolve(url, 'NS')):
         return False
     else:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+5
         return True
 #12
-def checkDomainRank(url):
+def checkDomainRank(url):       #critical
     global suspectScore
     url=tldextract.extract(url).domain+"."+tldextract.extract(url).suffix
     url='http://data.alexa.com/data?cli=100&dat=s&url='+str(url)
@@ -152,34 +157,34 @@ def checkDomainRank(url):
         if(int(dict_data['ALEXA']['SD'][1]['REACH']['@RANK'])<100000):
             return False
         elif(int(dict_data['ALEXA']['SD'][1]['REACH']['@RANK'])>100000):
-            suspectScore=suspectScore+1
+            suspectScore=suspectScore+5
             return True
     except:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+2
         return True
 #13
-def checkRedirection(url):
+def checkRedirection(url):     
     global suspectScore
     r = requests.get(url, allow_redirects=True)
     if(len(r.history)<=1):
         return False
     elif(len(r.history)>=2 and len(r.history)<4):
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+2.5
         return True
     else:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+5
         return True
 #14
-def checkMaliciousIframe(url):
+def checkMaliciousIframe(url):      #critical
     global suspectScore
     r = requests.get(url)
     if('frameborder="0"' in r.text):
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+5
         return True
     else:
         return False
 #15
-def checkRequestURL(url):
+def checkRequestURL(url):     
     global suspectScore
     domain = (tldextract.extract(url)).domain + "." + (tldextract.extract(url)).suffix
     response = requests.get(url)
@@ -204,11 +209,11 @@ def checkRequestURL(url):
         suspectScore=suspectScore+1
         return True
     else:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+4
         return True
 #16
 
-def checkPhishDatabase(url):
+def checkPhishDatabase(url):        #most critical
     global suspectScore
     ti=time.ctime(os.path.getmtime("ALL-phishing-domains.tar.gz"))
     date_time_obj = datetime.datetime.strptime(ti, '%a %b %d %H:%M:%S %Y')
@@ -227,30 +232,72 @@ def checkPhishDatabase(url):
     lines = set(phishdomain.read().splitlines())
 
     if domain in lines:
-        suspectScore=suspectScore+1
+        suspectScore=suspectScore+5
         return True
     else:
         return False
 
 #17
-def checkSlashRedirection(url):
+def checkSlashRedirection(url):     #non critical
     global suspectScore
     count=len(re.findall("//",url))
     if count>1:
+        suspectScore=suspectScore+2
         return True   
     else:
         return False
 
 #18
-def checkSubdomainCount(url):
+def checkSubdomainCount(url):       #non critical
     global suspectScore
     if(tldextract.extract(url).subdomain):
         url=(tldextract.extract(url).subdomain).split(".")
-        print(url)
         subdomain=len(url)
         if subdomain>1:
+            suspectScore=suspectScore+2
             return True
         else:
             return False
     else:
         return False
+
+def run(url):
+    global suspectScore
+    summary = []
+    if checkIp(url):
+        summary.append("IP address is used as an alternative of the domain name in the URL, website containing IP address are generally phishing, trying to steal their personal information.")
+    if checkUrlLength(url):
+        summary.append("Phishing websites uses long URL to hide the doubtful part in the address bar.")
+    if checkUrlSymbol(url):
+        summary.append("Phishing websites generally contains symbols such as using “@” symbol in the URL leads the browser to ignore everything preceding the “@” symbol and the real address often follows the “@” symbol.")
+    if checkDomainHyphen(url):
+        summary.append("The dash symbol is rarely used in legitimate URLs. Phishers tend to add prefixes or suffixes separated by (-) to the domain name so that users feel that they are dealing with a legitimate webpage. ")
+    if checkDomainAge(url):
+        summary.append("Phishing websites mostly live for a short period of time. By reviewing our dataset, we find that the minimum age of the legitimate domain is 6 months")
+    if checkDomainExpiry(url):
+        summary.append("Based on the fact that a phishing website lives for a short period of time, we believe that trustworthy domains are regularly paid for several years in advance. In our dataset, we find that the longest fraudulent domains have been used for one year only.")
+    if checkFaviconSource(url):
+        summary.append("If the favicon is loaded from a domain other than that shown in the address bar, then the webpage is likely to be considered a Phishing attempt. ")
+    if checkShortenUrl(url):
+        summary.append("Mostly phishing website uses url shortening method.")
+    if checkOpenPorts(url):
+        summary.append("Several firewalls, Proxy and Network Address Translation (NAT) servers will, by default, block all or most of the ports and only open the ones selected. If all ports are open there is high chance og phishing website.")
+    if checkHttpsInDomain(url):
+        summary.append("The existence of HTTPS is very important in giving the impression of website legitimacy.")
+    # if checkDNSRecord(url):
+    #     result.append("For phishing websites, either the claimed identity is not recognized by the WHOIS database or no records founded for the hostname. If the DNS record is empty or not found then the website is considered as “Phishing”.")
+    if checkDomainRank(url):
+        summary.append("Checking the domain rank using Alexa. Websites having the alexa rank more than a lakh are generally phishing.")
+    if checkRedirection(url):
+        summary.append("The fine line that distinguishes phishing websites from legitimate ones is how many times a website has been redirected, phishing websites containing this feature have been redirected at least 4 times. ")
+    if checkMaliciousIframe(url):
+        summary.append("IFrame is an HTML tag used to display an additional webpage into one that is currently shown. Phishers can make use of the “iframe” tag and make it invisible i.e. without frame borders. In this regard, phishers make use of the “frameBorder” attribute which causes the browser to render a visual delineation.")
+    if checkRequestURL(url):
+        summary.append("Request URL examines whether the external objects contained within a webpage such as images, videos and sounds are loaded from another domain. In Phishing webpages, the webpage address and most of objects embedded within the webpage are from the external domain.")
+    if checkPhishDatabase(url):
+        summary.append("Checking the url in the databases containing list of phishing websites. ")
+    if checkSlashRedirection(url):
+        summary.append("The existence of “//” within the URL path means that the user will be redirected to another website. If the Position of the Last Occurrence of "//" in the URL > 7 → Phishing.")
+    if checkSubdomainCount(url):
+        summary.append("If the website contains more than one sub-domain then there is a high chance of phishing website.")
+    return suspectScore, summary
